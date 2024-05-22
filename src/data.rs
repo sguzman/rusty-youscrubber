a559aa233352fa3422c2c8ece91591785ce458d9
Salvador Guzman <guzmansalv@gmail.com>
2024-05-22 12:57:37 -0700

    refactor: Update struct names and add new fields in data.rs

--- src/data.rs
+++ src/data.rs
@@ -82,6 +82,37 @@ pub struct Subtitle {
     pub url: Option<String>,
 }
 
+// pub Struct to represent the requested_downloads field in the json files
+#[derive(Debug, Deserialize, Serialize, Clone, PartialEq, Default)]
+pub struct RequestedDownload {
+    #[serde(rename = "__write_download_archive")]
+    pub write_download_archive: Option<bool>,
+
+    pub _filename: Option<String>,
+    pub abr: Option<f32>,
+    pub acodec: Option<String>,
+    pub aspect_ratio: Option<f32>,
+    pub asr: Option<u32>,
+    pub audio_channels: Option<u8>,
+    pub dynamic_range: Option<String>,
+    pub ext: Option<String>,
+    pub filename: Option<String>,
+    pub filesize_approx: Option<u64>,
+    pub format: Option<String>,
+    pub format_id: Option<String>,
+    pub format_note: Option<String>,
+    pub fps: Option<f32>,
+    pub height: Option<u32>,
+    pub language: Option<String>,
+    pub protocol: Option<String>,
+    pub requested_formats: Option<Vec<Format>>,
+    pub resolution: Option<String>,
+    pub tbr: Option<f32>,
+    pub vbr: Option<f32>,
+    pub vcodec: Option<String>,
+    pub width: Option<u32>,
+}
+
 // pub Struct to represent the entry field in the json files
 #[derive(Debug, Deserialize, Serialize, Clone, PartialEq, Default)]
 pub struct Video {
@@ -146,7 +177,7 @@ pub struct Video {
     pub protocol: Option<String>,
     pub release_date: Option<String>,
     pub release_year: Option<u32>,
-    //pub requested_downloads: Option<Vec<String>>,
+    pub requested_downloads: Option<Vec<RequestedDownload>>,
     //pub requested_formats: Option<Vec<String>>,
     //pub requested_subtitles: Option<Vec<String>>,
     pub resolution: Option<String>,
