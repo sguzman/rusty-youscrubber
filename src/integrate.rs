8d5756c2008bd82fdddb968572bb4dcbb333e3ea
Salvador Guzman <guzmansalv@gmail.com>
2024-05-23 12:22:54 -0700

    refactor: Update struct names and add new fields in integrate.rs, data.rs, and sql.rs

--- src/integrate.rs
+++ src/integrate.rs
@@ -1,12 +1,9 @@
 use log::{debug, info};
-use sea_orm::prelude::DateTime;
-use sea_orm::ActiveValue::{NotSet, Set};
-use sea_orm::{
-    ActiveModelTrait, ActiveValue, ConnectOptions, ConnectionTrait, Database, DatabaseConnection,
-};
+
+use sea_orm::{ConnectOptions, ConnectionTrait, Database, DatabaseConnection};
 use sea_orm::{Schema, Statement};
 
-use crate::sea_orm_models::payload::ActiveModel;
+use crate::constructors as ctor;
 
 use crate::data;
 
@@ -21,7 +18,11 @@ async fn db_connect() -> DatabaseConnection {
 
     debug!("{:#?}", opt);
 
-    Database::connect(opt).await.unwrap()
+    let db = Database::connect(opt).await.unwrap();
+    info!("Connected to database");
+    create_table(&db).await;
+
+    db
 }
 
 async fn create_table(db: &DatabaseConnection) {
@@ -42,88 +43,7 @@ async fn create_table(db: &DatabaseConnection) {
     }
 }
 
-// If option set to None, set to NotSet
-pub fn set<T>(option: Option<T>) -> ActiveValue<T>
-where
-    sea_orm::Value: From<T>,
-{
-    match option {
-        Some(value) => Set(value),
-        None => NotSet,
-    }
-}
-
-fn setui(option: Option<u64>) -> ActiveValue<Option<i32>> {
-    match option {
-        Some(value) => Set(Some(value as i32)),
-        None => NotSet,
-    }
-}
-
-fn setsi(option: Option<String>) -> ActiveValue<Option<i32>> {
-    match option {
-        Some(value) => Set(Some(value.parse::<i32>().unwrap())),
-        None => NotSet,
-    }
-}
-
-fn setus(option: Option<u64>) -> ActiveValue<Option<sea_orm::prelude::DateTime>> {
-    match option {
-        // Do not warn about deprecated usage
-        #[allow(deprecated)]
-        Some(value) => Set(Some(DateTime::from_timestamp(value as i64, 0).into())),
-        None => NotSet,
-    }
-}
-
-fn setsu(option: Option<String>) -> ActiveValue<Option<sea_orm::prelude::DateTime>> {
-    match option {
-        // Do not warn about deprecated usage
-        #[allow(deprecated)]
-        Some(value) => Set(Some(
-            DateTime::from_timestamp(value.parse::<i64>().unwrap(), 0).into(),
-        )),
-        None => NotSet,
-    }
-}
-
-fn setu3i(option: Option<u32>) -> ActiveValue<Option<i32>> {
-    match option {
-        Some(value) => Set(Some(value as i32)),
-        None => NotSet,
-    }
-}
-
-pub async fn create(payload: data::Channel) {
+pub async fn insert(payload: data::Channel) {
     let db = db_connect().await;
-
-    let a = ActiveModel {
-        channel: set(payload.channel),
-        channel_follower_count: setui(payload.channel_follower_count),
-        channel_id: set(payload.channel_id),
-        channel_url: Set(payload.channel_url),
-        availability: setsi(payload.availability),
-        description: Set(payload.description),
-        epoch: setus(payload.epoch),
-        extractor: Set(payload.extractor),
-        extractor_key: Set(payload.extractor_key),
-        payload_id: set(payload.id),
-        modified_date: setsu(payload.modified_date),
-        original_url: Set(payload.original_url),
-        playlist_count: setui(payload.playlist_count),
-        release_year: setu3i(payload.release_year),
-        title: set(payload.title),
-        type_of: Set(Some(payload.type_of)),
-        uploader: set(payload.uploader),
-        uploader_id: set(payload.uploader_id),
-        uploader_url: Set(payload.uploader_url),
-        view_count: setui(payload.view_count),
-        webpage_url: Set(payload.webpage_url),
-        webpage_url_basename: Set(payload.webpage_url_basename),
-        webpage_url_host: Set(payload.webpage_url_domain),
-        id: NotSet,
-    };
-
-    create_table(&db).await;
-    a.insert(&db).await.unwrap();
+    ctor::payload::create(&db, payload).await;
 }
