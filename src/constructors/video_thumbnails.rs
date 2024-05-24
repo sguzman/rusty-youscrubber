461737f26ccf3f662fc3f207bf3968594beed49a
Salvador Guzman <guzmansalv@gmail.com>
2024-05-23 23:07:06 -0700

    refactor: Integrate video_thumbnails module into payload.rs and constructors

--- src/constructors/video_thumbnails.rs
+++ src/constructors/video_thumbnails.rs
@@ -3,7 +3,7 @@ use crate::sea_orm_models as sea;
 use log::{debug, warn};
 use sea_orm::{ActiveModelTrait, DatabaseConnection};
 
-use sea_orm::ActiveValue::{Set};
+use sea_orm::ActiveValue::Set;
 
 pub async fn create(db: &DatabaseConnection, video_id: i32, vtso: Option<Vec<VideoThumbnail>>) {
     if let Some(vts) = vtso {
