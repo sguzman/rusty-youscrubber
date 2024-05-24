8c577bfd58f3e73ef9a76af8f955f5f78e764b2a
Salvador Guzman <guzmansalv@gmail.com>
2024-05-24 00:02:02 -0700

    refactor: Integrate video_thumbnails, requested_download, formats, subtitle_type, chapters, automatic_captions, video_categories, and video_tags modules into payload.rs and constructors

--- src/constructors/subtitles.rs
+++ src/constructors/subtitles.rs
@@ -1,20 +1,22 @@
 use crate::sea_orm_models as sea;
-use sea_orm::{ActiveModelTrait, DatabaseConnection};
+use sea_orm::entity::*;
+use sea_orm::DatabaseConnection;
 
 use crate::data;
 use sea_orm::ActiveValue::{NotSet, Set};
 
 pub async fn create(db: &DatabaseConnection, video_id: i32, ac_id: i32, acs: Vec<data::Subtitle>) {
-    for ac in acs {
-        let tag = sea::subtitle::ActiveModel {
-            id: NotSet,
-            ext: Set(ac.ext),
-            url: Set(ac.url),
-            video_id: Set(video_id.try_into().unwrap()),
-            name: Set(ac.name),
-            subtitle_type_id: Set(ac_id.try_into().unwrap()),
-        };
+    let all = acs.into_iter().map(|ac| sea::subtitle::ActiveModel {
+        id: NotSet,
+        ext: Set(ac.ext),
+        url: Set(ac.url),
+        video_id: Set(video_id.try_into().unwrap()),
+        name: Set(ac.name),
+        subtitle_type_id: Set(ac_id.try_into().unwrap()),
+    });
 
-        tag.insert(db).await.unwrap();
-    }
+    sea::subtitle::Entity::insert_many(all)
+        .exec(db)
+        .await
+        .expect("Error creating captions");
 }
--- src/constructors/video_tags.rs
+++ src/constructors/video_tags.rs
@@ -1,18 +1,22 @@
 use crate::sea_orm_models as sea;
-use sea_orm::{ActiveModelTrait, DatabaseConnection};
+use sea_orm::DatabaseConnection;
+use sea_orm::entity::*;
 
 use sea_orm::ActiveValue::{NotSet, Set};
 
 pub async fn create(db: &DatabaseConnection, video_id: i32, tags: Option<Vec<String>>) {
     if let Some(tags) = tags {
-        for tag in tags {
-            let tag = sea::videotag::ActiveModel {
+        let all = tags.into_iter().map(|tag|  {
+            sea::videotag::ActiveModel {
                 id: NotSet,
                 video_id: Set(video_id.try_into().unwrap()),
                 tag: Set(Some(tag)),
-            };
+            }
+        });
 
-            tag.insert(db).await.unwrap();
-        }
+        sea::videotag::Entity::insert_many(all)
+            .exec(db)
+            .await
+            .expect("Error creating video tags");
     }
 }
