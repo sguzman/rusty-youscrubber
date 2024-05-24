5eac696fe6e98f3d8d143c2b49d525e988f1db54
Salvador Guzman <guzmansalv@gmail.com>
2024-05-23 23:58:17 -0700

    refactor: Integrate video_thumbnails module into payload.rs and constructors

--- src/constructors/video_thumbnails.rs
+++ src/constructors/video_thumbnails.rs
@@ -1,29 +1,23 @@
 use crate::data::VideoThumbnail;
 use crate::sea_orm_models as sea;
-use log::{debug, warn};
-use sea_orm::{ActiveModelTrait, DatabaseConnection};
+use sea_orm::entity::*;
+use sea_orm::DatabaseConnection;
 
 use sea_orm::ActiveValue::{NotSet, Set};
 
 pub async fn create(db: &DatabaseConnection, video_id: i32, vtso: Option<Vec<VideoThumbnail>>) {
     if let Some(vts) = vtso {
-        for vt in vts {
-            let v = sea::videothumbnail::ActiveModel {
-                id: NotSet,
-                video_id: Set(video_id),
-                preference: Set(vt.preference),
-                thumbnail_id: Set(vt.id),
-                url: Set(vt.url),
-            };
+        let all = vts.into_iter().map(|vt| sea::videothumbnail::ActiveModel {
+            id: NotSet,
+            video_id: Set(video_id),
+            preference: Set(vt.preference),
+            thumbnail_id: Set(vt.id),
+            url: Set(vt.url),
+        });
 
-            let result = v.insert(db).await;
-
-            match result {
-                Ok(_) => debug!("VideoThumbnail created"),
-                Err(e) => {
-                    warn!("Error: {}", e);
-                }
-            }
-        }
+        sea::videothumbnail::Entity::insert_many(all)
+            .exec(db)
+            .await
+            .expect("Error creating captions");
     }
 }
